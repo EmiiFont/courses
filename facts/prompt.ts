ddfc5ce30d02fa187c70a23cb40884b1db3bde29
Emilio <emifont01@gmail.com>
2024-03-24 23:35:14 -0400

    refactor: rename

--- facts/prompt.ts
+++ facts/prompt.ts
@@ -2,7 +2,6 @@ import { RetrievalQAChain } from "langchain/chains";
 import { Chroma } from "@langchain/community/vectorstores/chroma";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { ChatOpenAI } from "langchain/chat_models/openai";
-import { RedudantFilterRetriever } from "./RedudantFilterRetriever";
 
 const chat = new ChatOpenAI({});
 const embeddings = new OpenAIEmbeddings();
